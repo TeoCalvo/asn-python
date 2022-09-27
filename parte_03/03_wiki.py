587a74af0262e7654427b0f357d1b02f8f75b75e
Teo Calvo <teocalvo2@gmail.com>
2022-09-26 21:41:28 -0300

    Final aula 04

--- parte_03/03_wiki.py
+++ parte_03/03_wiki.py
@@ -11,4 +11,9 @@ print(data)
 file_open = open("wikipedia_python.html", 'w')
 file_open.write(data)
 file_open.close()
+
 # %%
+
+with open("wikipedia_python_2.html", 'w') as open_file:
+    file_open.write(data)
+
